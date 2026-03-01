4f506910523ff077c7d3c3b7f44dcd67d458c74b
ale <ale@manalejandro.com>
2026-03-01 03:12:59 +0100

    fix streaming batch
    Signed-off-by: ale <ale@manalejandro.com>

--- api/views/streaming.py
+++ api/views/streaming.py
@@ -100,6 +100,10 @@ async def event_stream_generator(
                     queryset = queryset.filter(id__gt=int(last_event_id))
                 
                 events = await sync_to_async(list)(queryset.order_by("id")[:20])
+                if events:
+                    # Wait briefly so messages arriving close together are batched
+                    await asyncio.sleep(1)
+                    events = await sync_to_async(list)(queryset.order_by("id")[:20])
                 
                 for event in events:
                     last_event_id = str(event.id)
@@ -130,6 +134,10 @@ async def event_stream_generator(
                     queryset = queryset.filter(id__gt=int(last_event_id))
                 
                 posts = await sync_to_async(list)(queryset.order_by("id")[:20])
+                if posts:
+                    # Wait briefly so messages arriving close together are batched
+                    await asyncio.sleep(1)
+                    posts = await sync_to_async(list)(queryset.order_by("id")[:20])
                 
                 for post in posts:
                     last_event_id = str(post.id)
